eb9e27b0590ab60988d9b469cfe69a10eaa45a32
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-10-29 15:09:17 +0900

    ログアウト後のログインページにはログアウトしたことが分かるメッセージを表示しよう

--- src/main/java/com/example/securedapp/config/SecurityConfig.java
+++ src/main/java/com/example/securedapp/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.securedapp.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -14,6 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(new LoginLogoutRequestMatcher()).permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(login -> login
@@ -24,8 +27,20 @@ public class SecurityConfig {
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout") // この設定はデフォルト値なので省略可
+                        .logoutSuccessUrl("/login?logout") // この設定はデフォルト値なので省略可
                 );
 
         return http.build();
+    }
+
+    /**
+     * リクエストが {@code /login?logout} かどうかを判定する RequestMatcher
+     */
+    private static class LoginLogoutRequestMatcher implements RequestMatcher {
+        @Override
+        public boolean matches(HttpServletRequest request) {
+            return "/login".equals(request.getServletPath())
+                    && "".equals(request.getParameter("logout"));
+        }
     }
 }
