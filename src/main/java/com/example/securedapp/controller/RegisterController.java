7ca7e0787e6a87fa128bf7fbfe117d06aeffb353
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-11-18 23:10:05 +0900

    バリデーションエラーメッセージを表示しよう

--- src/main/java/com/example/securedapp/controller/RegisterController.java
+++ src/main/java/com/example/securedapp/controller/RegisterController.java
@@ -1,13 +1,18 @@
 package com.example.securedapp.controller;
 
+import com.example.securedapp.controller.user.UserForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RegisterController {
 
     @GetMapping("/register")
-    public String show() {
+    public String show(Model model) {
+        if (!model.containsAttribute("userForm")) {
+            model.addAttribute(new UserForm(null, null));
+        }
         return "register";
     }
 }
--- src/main/java/com/example/securedapp/controller/user/UserController.java
+++ src/main/java/com/example/securedapp/controller/user/UserController.java
@@ -2,10 +2,12 @@ package com.example.securedapp.controller.user;
 
 import com.example.securedapp.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,8 +16,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public String create(@Validated UserForm form, BindingResult bindingResult) {
+    public String create(
+            @Validated UserForm form,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes
+    ) {
         if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute(form);
+            redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + Conventions.getVariableName(form), bindingResult);
             return "redirect:/register";
         }
         userService.create(form.username(), form.password());
