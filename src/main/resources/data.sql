d66df84da9d1ad3ce4892aea08eb5806d4106c72
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-11-19 00:24:21 +0900

    エンコードしたパスワードを初期データとして投入しよう

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO users (username, password) VALUES ('tom', 'password');
-INSERT INTO users (username, password) VALUES ('bob', 'password');
+-- password = password1234
+INSERT INTO users (username, password) VALUES ('tom', '$2a$10$iDFub7VH/DFDtUF6AFPXuuTDHI56oXWdXz.7F3L7xgUTFh004InNm');
+-- password = password1234
+INSERT INTO users (username, password) VALUES ('bob', '$2a$10$Uz3R17Fn4izSBDO15I5TpeEgb0ugusFqdHfxqic.7DbEMFzL8pW4O');
