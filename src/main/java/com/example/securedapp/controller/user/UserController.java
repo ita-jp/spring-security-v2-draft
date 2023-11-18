4aaf8e4370c673a41c534baf840f091e99bdf350
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-11-18 22:00:01 +0900

    @RequestParam：フォームから入力値を送信してサーバーで受け取ろう

--- src/main/java/com/example/securedapp/controller/user/UserController.java
+++ src/main/java/com/example/securedapp/controller/user/UserController.java
@@ -2,12 +2,16 @@ package com.example.securedapp.controller.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
 
     @PostMapping("/users")
-    public String create() {
+    public String create(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password
+    ) {
         return "redirect:/login";
     }
 }
