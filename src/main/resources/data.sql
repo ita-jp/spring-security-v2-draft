256f9ad9124ef35af0ccda2870318e26e3083a9e
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-11-19 18:00:41 +0900

    準備：お知らせのテーブルを作成して初期データを登録しよう

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,6 @@
 INSERT INTO users (username, password) VALUES ('tom', '$2a$10$iDFub7VH/DFDtUF6AFPXuuTDHI56oXWdXz.7F3L7xgUTFh004InNm');
 -- password = password1234
 INSERT INTO users (username, password) VALUES ('bob', '$2a$10$Uz3R17Fn4izSBDO15I5TpeEgb0ugusFqdHfxqic.7DbEMFzL8pW4O');
+
+INSERT INTO notifications(username, message) VALUES ('tom', 'これはテストのおしらせです。1');
+INSERT INTO notifications(username, message) VALUES ('bob', 'これはテストのおしらせです。2');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users
     username VARCHAR(50)  NOT NULL PRIMARY KEY,
     password VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE notifications
+(
+    id         BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    message    VARCHAR(100) NOT NULL,
+    username   VARCHAR(50)  NOT NULL REFERENCES users (username),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
