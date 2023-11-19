b118b7f5714438f83d0eac53a8c17820716bfb59
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-11-19 18:14:43 +0900

    準備：お知らせ登録リクエストを受け取るハンドラーメソッドを定義しよう

--- src/main/java/com/example/securedapp/controller/notification/NotificationController.java
+++ src/main/java/com/example/securedapp/controller/notification/NotificationController.java
@@ -2,6 +2,7 @@ package com.example.securedapp.controller.notification;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class NotificationController {
@@ -9,5 +10,10 @@ public class NotificationController {
     @GetMapping("/notifications/creationForm")
     public String showCreationForm() {
         return "notification/creationForm";
+    }
+
+    @PostMapping("/notifications")
+    public String create() {
+        return "redirect:/";
     }
 }
